3758fdca8480402adf19bf1f0a94b313ba1e74a5
Yuri D'Elia <wavexx@thregr.org>
2020-07-01 17:07:31 +0200

    Fix regressions from 391f71b72295c47c5de130ea1cbd5fef3f21c1f5
    - "stage" would fail to bootstrap empty branches
    - "stage" would recreate branches with no changes
    
    Add basic stage tests

---
+++ t/t0201-stage_basics.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+DESC="basic stage tests"
+. ./lib.sh
+
+verb "initialize a new repo"
+git init -q
+touch file_master
+git add file_master
+commit
+
+verb "initialize assembly file with a stage branch"
+cat <<EOF > .gitassembly
+stage test master
+EOF
+
+verb "ensure stage can bootstrap the branch"
+capture gas -av
+assert_out_regex "creating branch test from master"
+checkout test
+test -f file_master
+
+verb "stage doesn't recreate branches for no reason"
+capture gas -av
+assert_out_regex "already up to date"
